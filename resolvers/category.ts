780bfbf654bc1d0e9bd9f12e1fee9b9ac06096ee
Isuru Udukala <iudukala@gmail.com>
2023-03-21 21:39:20 +0530

    refactoring

--- resolvers/category.ts
+++ resolvers/category.ts
@@ -1,7 +1,11 @@
-import { products } from '../data.ts';
+import { ProductType } from '../data.ts';
 
 export const Category = {
-	products: (parent: { id: string; name: string }, args: { id: string }, context: object) => {
-		return products.filter((product: { categoryID: string }) => product.categoryID === parent.id);
+	products: (
+		parent: { id: string; name: string },
+		args: { id: string },
+		context: { products: Array<ProductType> },
+	) => {
+		return context.products.filter((product: { categoryID: string }) => product.categoryID === parent.id);
 	},
 };
--- resolvers/query.ts
+++ resolvers/query.ts
@@ -1,14 +1,18 @@
-import { products, categories } from '../data.ts';
+import { ProductType, CategoryType } from '../data.ts';
 
 export const Query = {
 	hello: () => ['World!'],
 
-	products: () => products,
-	product: (parent: object, args: { id: string }, context: object) => {
-		return products.find((product: { id: string }) => product.id === args.id) || null;
+	products: (parent: object, args: { id: string }, context: { products: Array<ProductType> }) =>
+		context.products,
+	product: (parent: object, args: { id: string }, context: { products: Array<ProductType> }) => {
+		return context.products.find((product: { id: string }) => product.id === args.id) || null;
 	},
-	categories: () => categories,
-	category: (parent: object, args: { id: string }, context: object) => {
-		return categories.find((category: { id: string }) => category.id === args.id) || null;
+
+	categories: (parent: object, args: { id: string }, context: { categories: Array<CategoryType> }) =>
+		context.categories,
+	// category: (parent: object, args: { id: string }, context: object) => {
+	category: (parent: object, args: { id: string }, context: { categories: Array<CategoryType> }) =>{
+		return context.categories.find((category: { id: string }) => category.id === args.id) || null;
 	},
 };
