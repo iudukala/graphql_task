e80887b78b4348e08bfeabd51ad48c9e4685c6f9
Isuru Udukala <iudukala@gmail.com>
2023-04-02 18:44:36 +0530

    switched unnamed default export to named export to avoid unnamed stacktrace errors

--- src/graphql/schema_graphql_nexus.ts
+++ src/graphql/schema_graphql_nexus.ts
@@ -4,7 +4,7 @@ import * as nexusTypes from './nexus_types';
 
 const AUTOGEN_DIR = 'nexus_autogen';
 
-export default makeSchema({
+const nexusSchema = makeSchema({
 	types: nexusTypes,
 	outputs: {
 		typegen: join(__dirname, AUTOGEN_DIR, 'nexus-typegen.ts'),
@@ -14,3 +14,5 @@ export default makeSchema({
 	// prettier config to apply to output artifacts
 	prettierConfig: join(__dirname, '../../.prettierrc'),
 });
+
+export default nexusSchema;
