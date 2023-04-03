db6d5021dc1b200f7c5eece5a2ac6d2a32c1e40d
Isuru Udukala <iudukala@gmail.com>
2023-04-03 11:04:31 +0530

    added type definition for gql nexus methods

--- src/graphql/nexus_types/FruitMutation.ts
+++ src/graphql/nexus_types/FruitMutation.ts
@@ -1,15 +1,19 @@
 import { extendType, stringArg, intArg, nonNull } from 'nexus';
 import { GQLNexusTypeName } from '../enum_nexus_type_keys';
 import { NexusGenObjects } from '../nexus_autogen/nexus-typegen';
+import { GQL_IDKEY } from '../const/gqlIDKey';
+import { AllNexusArgsDefs } from 'nexus/dist/core';
 
-type FruitMutationType = Omit<NexusGenObjects[GQLNexusTypeName.Fruit], 'id'>;
-
+type FruitMutationType = Record<
+	keyof Omit<NexusGenObjects[GQLNexusTypeName.Fruit], typeof GQL_IDKEY>,
+	AllNexusArgsDefs
+>;
 export const FruitMutation = extendType({
 	type: 'Mutation',
 	definition(t) {
 		t.nonNull.field('createFruit', {
 			type: GQLNexusTypeName.Fruit,
-			args: {
+			args: <FruitMutationType>{
 				name: nonNull(stringArg()),
 				description: nonNull(stringArg()),
 				amount: nonNull(intArg()),
