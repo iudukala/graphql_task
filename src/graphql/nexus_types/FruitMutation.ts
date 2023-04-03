fc11c4ef7a0be7332d2e826b89a3750e9cd67073
Isuru Udukala <iudukala@gmail.com>
2023-04-03 10:37:41 +0530

    adding mutator: fruit

---
+++ src/graphql/nexus_types/FruitMutation.ts
@@ -0,0 +1,21 @@
+import { extendType, stringArg, nonNull } from 'nexus';
+import { GQLNexusTypeName } from '../enum_nexus_type_keys';
+import { NexusGenObjects } from '../nexus_autogen/nexus-typegen';
+
+type FruitMutationType = Omit<NexusGenObjects[GQLNexusTypeName.Fruit], 'id'>;
+
+export const FruitMutation = extendType({
+	type: 'Mutation',
+	definition(t) {
+		t.nonNull.field('createFruit', {
+			type: GQLNexusTypeName.Fruit,
+			args: {
+				name: nonNull(stringArg()),
+				description: nonNull(stringArg()),
+			},
+			resolve: () => {
+				return null;
+			},
+		});
+	},
+});
