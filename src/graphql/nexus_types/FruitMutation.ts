de385bbf110259fb5f5c339d84b06ade02c453bb
Isuru Udukala <iudukala@gmail.com>
2023-04-03 12:24:51 +0530

    build fruit mutation

--- src/graphql/nexus_types/FruitMutation.ts
+++ src/graphql/nexus_types/FruitMutation.ts
@@ -3,24 +3,30 @@ import { GQLNexusTypeName } from '../enum_nexus_type_keys';
 import { NexusGenObjects } from '../nexus_autogen/nexus-typegen';
 import { GQL_IDKEY } from '../const/gqlIDKey';
 import { AllNexusArgsDefs } from 'nexus/dist/core';
+import { randomUUID } from 'crypto';
+import { context } from '../../gql_context';
+import { GQLContextType } from '../..';
 
 // todo: replace with generic type that builds the type for mutator arg object
-type FruitMutationType = Record<
-	keyof Omit<NexusGenObjects[GQLNexusTypeName.Fruit], typeof GQL_IDKEY>,
-	AllNexusArgsDefs
->;
+type IDOmittedFruitType = Omit<NexusGenObjects[GQLNexusTypeName.Fruit], typeof GQL_IDKEY>;
 export const FruitMutation = extendType({
 	type: 'Mutation',
 	definition(t) {
 		t.nonNull.field('createFruit', {
 			type: GQLNexusTypeName.Fruit,
-			args: <FruitMutationType>{
+			args: <Record<keyof IDOmittedFruitType, AllNexusArgsDefs>>{
 				name: nonNull(stringArg()),
 				description: nonNull(stringArg()),
 				amount: nonNull(intArg()),
 			},
-			resolve: () => {
-				return null;
+			resolve: (_, args: IDOmittedFruitType, context: GQLContextType) => {
+				const newFruit: NexusGenObjects[GQLNexusTypeName.Fruit] = {
+					[GQL_IDKEY]: randomUUID(),
+					...args,
+				};
+
+				context.fruits.push(newFruit);
+				return newFruit;
 			},
 		});
 	},
