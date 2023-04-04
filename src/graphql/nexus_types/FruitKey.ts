29cbc1d6f63a5ea044fb861a2392cea034f2e17c
Isuru Udukala <iudukala@gmail.com>
2023-04-04 13:23:54 +0530

    removed duplicated enum

--- src/graphql/nexus_types/FruitKey.ts
+++
@@ -1,9 +0,0 @@
-import { GQL_IDKEY } from '../constants/const_GQLIDKey';
-
-export enum FruitKey {
-	ID = GQL_IDKEY,
-	Name = 'name',
-	Description = 'description',
-	Limit = 'limit',
-	Amount = 'amount',
-}
