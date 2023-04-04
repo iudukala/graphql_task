8588d10f7d9967b22847aa89335d8f3a7f0d9e78
Isuru Udukala <iudukala@gmail.com>
2023-04-04 13:16:22 +0530

    adding enums to store entity field names (fruit)

---
+++ src/graphql/constants/enum_nexusTypeKey.ts
@@ -0,0 +1,5 @@
+export enum GQLType {
+	Query = 'Query',
+
+	Fruit = 'Fruit',
+}
---
+++ src/graphql/nexus_types/enum_FruitKey.ts
@@ -0,0 +1,9 @@
+import { GQL_IDKEY } from '../constants/GQL_IDKey';
+
+export enum FruitKey {
+	ID = GQL_IDKEY,
+	Name = 'name',
+	Description = 'description',
+	Limit = 'limit',
+	Amount = 'amount',
+}
