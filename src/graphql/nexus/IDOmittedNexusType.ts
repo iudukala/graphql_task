ead4ad63b7e9f097e9e1a144113e4a14b94c4d7b
Isuru Udukala <iudukala@gmail.com>
2023-04-05 03:13:39 +0530

    updated index jsdoc and removed unnecessary type IDOmittedNexusType

--- src/graphql/nexus/IDOmittedNexusType.ts
+++
@@ -1,5 +0,0 @@
-import { GQLType } from '../constants/enum_nexusTypeKey';
-import { NexusGenObjects } from '../nexus_autogen/nexus-typegen';
-import { GQL_IDKEY } from '../constants/const_GQLIDKey';
-
-export type IDOmittedNexusType<T extends GQLType> = Omit<NexusGenObjects[T], typeof GQL_IDKEY>;
--- src/graphql/nexus/index.ts
+++ src/graphql/nexus/index.ts
@@ -1,3 +1,7 @@
+/**
+ * contains the types and resolvers required for nexus to build the schema by calling makeSchema()
+ */
+
 export { FruitNX } from './types/Fruit';
 export { FruitQueryExt } from './operations/FruitQueryExt';
 export { createFruitForFruitStorage } from './operations/createFruitForFruitStorage';
