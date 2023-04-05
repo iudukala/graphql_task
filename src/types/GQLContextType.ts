94b773568f6abeeffce9920098494c7cada67ecb
Isuru Udukala <iudukala@gmail.com>
2023-04-05 16:24:57 +0530

    fixed import bug

--- src/types/GQLContextType.ts
+++ src/types/GQLContextType.ts
@@ -1,4 +1,4 @@
-import { NexusGenObjects } from '../graphql/nexus_autogen_artifacts/nexus_typegen.ts';
+import { NexusGenObjects } from '../graphql/nexus_autogen_artifacts/nexus_typegen';
 import { GQLType } from '../graphql/constants/enum_nexusTypeKey';
 
 export type GQLContextType = {
