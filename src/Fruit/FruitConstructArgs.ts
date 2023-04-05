e4a88564cbc34d7108885d0b4ece1a749ae5d88e
Isuru Udukala <iudukala@gmail.com>
2023-04-06 00:14:12 +0530

    set flag isolatedModules: true

--- src/Fruit/FruitConstructArgs.ts
+++ src/Fruit/FruitConstructArgs.ts
@@ -1,7 +1,7 @@
 import { FruitKey } from '../graphql/constants/enum_fruitKey';
 import type { FruitType } from '../graphql/nexus_types/FruitType';
 
-
 export type FruitConstructArgs = Pick<
-	FruitType, FruitKey.Name | FruitKey.Description | FruitKey.Limit
+	FruitType,
+	FruitKey.Name | FruitKey.Description | FruitKey.Limit
 >;
