9ddc27a6ee9b3c7f594731dfc364badc17c11d55
Isuru Udukala <iudukala@gmail.com>
2023-04-06 16:02:33 +0530

    fixing fruit id:name issue

--- src/core/Fruit.ts
+++ src/core/Fruit.ts
@@ -1,10 +1,19 @@
 import { FruitKey } from '../graphql/constants/enum_fruitKey';
 import { FruitType } from '../graphql/nexus_types/FruitType';
 import { Entity } from './Entity';
+import _ from 'lodash';
 
+type ModifiedFruitType = Omit<FruitType, 'id'>;
 class Fruit extends Entity<FruitType> {
-	constructor(propsGQL: FruitType) {
-		super(propsGQL.name, { ...propsGQL, [FruitKey.Name]: undefined });
+	constructor(propsFruitGQL: FruitType) {
+		// super(propsGQL.name, { ...propsGQL, [FruitKey.Name]: undefined });
+		const { name, ...fruitProps } = _.cloneDeep(propsFruitGQL);
+		super(propsGQL.name, fruitProps);
+		// super(propsGQL.name, propsGQL);
+		// _.cloneDeepWith(propsGQL, (_, key: string | number | undefined) => {
+		//     if
+		// }))
+		// );
 	}
 }
 
