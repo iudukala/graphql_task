439c8eebe1bd4261f20c8ead7b46139a28982d5e
Isuru Udukala <iudukala@gmail.com>
2023-04-06 16:56:07 +0530

    snapshot

--- src/core/Fruit.ts
+++ src/core/Fruit.ts
@@ -1,16 +1,13 @@
-import { FruitKey } from '../graphql/constants/enum_fruitKey';
 import { FruitType } from '../graphql/nexus_types/FruitType';
 import { Entity } from './Entity';
-import _ from 'lodash';
 
-// type ModifiedFruitType = Omit<FruitType, 'name'>;
-// class Fruit extends Entity<ModifiedFruitType> {
+// import { FruitKey } from '../graphql/constants/enum_fruitKey';
+// import _ from 'lodash';
+
 class Fruit extends Entity<FruitType> {
 	constructor(propsFruitGQL: FruitType) {
-		// const { name, ...fruitProps } = _.cloneDeep(propsFruitGQL);
-
 		/**
-		 * since the object is immutable, the fact that there are now two references to the field 'name' is not an issue and is also
+		 * since the object is immutable, the fact that there are now two references to the field 'name' is not an issue
 		 */
 		super(propsFruitGQL.name, propsFruitGQL);
 	}
