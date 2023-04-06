feb16b3a1d911ac6914c9c56323ecab68101c50e
Isuru Udukala <iudukala@gmail.com>
2023-04-06 18:43:34 +0530

    upgrading entity base class to fix id duplication issue

--- src/core/Entity.ts
+++ src/core/Entity.ts
@@ -1,8 +1,13 @@
 export abstract class Entity<T> {
-	protected readonly _id: string;
+	protected readonly _id: () => string;
 	protected props: T;
 
-	constructor(id: string, propsArg: T) {
+	/**
+	 *
+	 * @param {() => string} id a function that returns the identifier of the entity. a function is used instead of a constant value to avoid duplicating data in instances where the identifier may be a field that's inside the var props
+	 * @param propsArg object containing the data pertaining to the entity
+	 */
+	constructor(id: () => string, propsArg: T) {
 		this._id = id;
 		this.props = Object.freeze(propsArg);
 	}
