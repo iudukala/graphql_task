3ebcd588b661672854a62e1b6219bdda8287ff14
Isuru Udukala <iudukala@gmail.com>
2023-04-07 12:57:20 +0530

    switched to using omit<> utility type after switching from enum to const asserted object

--- src/Fruit/FruitConstructArgs.ts
+++ src/Fruit/FruitConstructArgs.ts
@@ -1,7 +1,4 @@
 import { FruitKey } from '../graphql/constants/enum_fruitKey';
 import type { FruitType } from '../graphql/nexus_types/FruitType';
 
-export type FruitConstructArgs = Pick<
-	FruitType,
-	typeof FruitKey.Name | typeof FruitKey.Description | typeof FruitKey.Limit
->;
+export type FruitConstructArgs = Omit<FruitType, typeof FruitKey.ID | typeof FruitKey.Amount>;
