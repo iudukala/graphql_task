e54a825dc8104176decbee2dda1ef09d697d0245
Isuru Udukala <iudukala@gmail.com>
2023-04-07 12:20:11 +0530

    switching enum to const asserted object

--- src/graphql/constants/enum_fruitKey.ts
+++ src/graphql/constants/enum_fruitKey.ts
@@ -1,9 +1,9 @@
 import { GQL_IDKEY } from './const_GQLIDKey';
 
-export const enum FruitKey {
-	ID = GQL_IDKEY,
-	Name = 'name',
-	Description = 'description',
-	Limit = 'limit',
-	Amount = 'amount',
-}
+export const FruitKey =  {
+	ID : GQL_IDKEY,
+	Name : 'name',
+	Description : 'description',
+	Limit : 'limit',
+	Amount : 'amount',
+} as const;
