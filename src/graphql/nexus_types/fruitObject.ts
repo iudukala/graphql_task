048acceb3e93e87091f6263cd3786042db6026df
Isuru Udukala <iudukala@gmail.com>
2023-04-08 15:17:05 +0530

    updated the type of the identifier to id

--- src/graphql/nexus_types/fruitObject.ts
+++ src/graphql/nexus_types/fruitObject.ts
@@ -4,7 +4,7 @@ import { GQLType } from '../constants/enum_nexusTypeKey';
 export const FruitNX = objectType({
 	name: GQLType.Fruit,
 	definition(t) {
-		t.nonNull.string('id');
+		t.nonNull.id('id');
 		t.nonNull.string('name');
 		t.string('description');
 
