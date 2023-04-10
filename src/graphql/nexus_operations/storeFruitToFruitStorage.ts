1a0cb374e90132c287384cc240364b7c5645af4d
Isuru Udukala <iudukala@gmail.com>
2023-04-10 12:52:37 +0530

    adding fruit increment mutation

---
+++ src/graphql/nexus_operations/storeFruitToFruitStorage.ts
@@ -0,0 +1,41 @@
+
+import { extendType, stringArg, intArg, nonNull } from 'nexus';
+import { GQLType } from '../constants/enum_nexusTypeKey.js';
+import { AllNexusArgsDefs } from 'nexus/dist/core.js';
+import { GQLContextType } from '../../types/GQLContextType.js';
+
+import type { FruitConstructArgs } from '../../Fruit/types.js';
+import { Fruit } from '../../Fruit/Fruit.js';
+import { translateFruit } from '../../Fruit/translateFruit.js';
+
+
+
+
+/**
+ * mutation for incrementing the count of an existing fruit.
+ * structure: storeFruitToFruitStorage(name: string, amount: int)
+ */
+export const storeFruitToFruitStorage = extendType({
+	type: 'Mutation',
+	definition(t) {
+		t.nonNull.field('storeFruitToFruitStorage', {
+			type: GQLType.Fruit,
+
+			args: <Record<keyof FruitConstructArgs, AllNexusArgsDefs>>{
+				name: nonNull(stringArg()),
+				description: nonNull(stringArg()),
+				limit: nonNull(intArg()),
+			},
+
+			resolve: (_, args: FruitConstructArgs, context: GQLContextType) => {
+				const newFruit: Fruit = Fruit.createNewFruit(args);
+
+				// todo: persistence logic
+				const translated = translateFruit(newFruit);
+
+				context.fruits.push(translated);
+				return translated;
+			},
+		});
+	},
+});
