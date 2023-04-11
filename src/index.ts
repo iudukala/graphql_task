7c0e20dafdf00c3ec401770c4343d11290e254d0
Isuru Udukala <iudukala@gmail.com>
2023-04-11 12:06:17 +0530

    imnplemeting mongo operations

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,36 @@ import { graphqlHTTP } from 'express-graphql';
 import { nexusSchema } from './graphql/schemaConfigNexus.js';
 import { contextGQL } from './graphql/common/contextGQL.js';
 
+// import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
+import mongodb from 'mongodb';
+import router from './restroute.js';
+
+dotenv.config();
+if (process.env.DB_URI === null || process.env.DB_URI === undefined)
+	throw new Error(' -- ' + process.env.DB_URI);
+
+// const client = await mongodb.MongoClient.connect(process.env.DB_URI, {
+const client = new mongodb.MongoClient(process.env.DB_URI);
+
+// maxPoolSize: 50,
+// catch(error => console.error(error));
+
+try {
+	// Connect to the MongoDB cluster
+	await client.connect();
+
+	const dblist = await client.db().admin().listDatabases();
+	console.log(dblist);
+
+	// Make the appropriate DB calls
+	// await listDatabases(client);
+} catch (e) {
+	console.error(e);
+} finally {
+	await client.close();
+}
+
 express()
 	.use(
 		'/graphql',
@@ -14,6 +44,17 @@ express()
 			pretty: false,
 		}),
 	)
+	.use('/r', router)
+	.use('*', (req, res) => res.status(404).json({ error: 'not found' }))
 	.listen(4000);
 
 console.log('running on :4000/graphql');
+
+// mongodb.MongoClient.connect(process.env.DB_URI, {
+// 	maxPoolSize: 50,
+// }).catch(error => console.error(error));
+// .then(async client => {
+// 	app.listen(port, () => {
+// 		console.log('listening on port 5000');
+// 	});
+// });
