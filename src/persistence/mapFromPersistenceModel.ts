0dd03743a7e31091f956e169ca7a138b8b93ee35
Isuru Udukala <iudukala@gmail.com>
2023-04-14 13:07:47 +0530

    jsdoc update

--- src/persistence/mapFromPersistenceModel.ts
+++ src/persistence/mapFromPersistenceModel.ts
@@ -1,14 +1,12 @@
-import mongoose from 'mongoose';
 import { Fruit } from '../Fruit/Fruit.js';
 import { FruitKey } from '../Fruit/enum_fruitKey.js';
-import { FruitTypeGQL } from '../graphql/nexus_types/FruitTypeGQLNX.js';
 import { PersistenceFruitModel } from './type_persistenceFruitModel.js';
 
 /**
  * takes a response object returned from mongoose and returns a fruit domain entity
  *
  * @param fruit object returned through mongoose
- * @returns a new fruit domain entity
+ * @returns {Fruit} a new fruit domain entity
  */
 export const mapFromPersistenceModel = (fruit: PersistenceFruitModel): Fruit => {
 	return Fruit.reconstituteFruit({
