87f47129e7ddedc6d3a8fb82685bd1f796e8b681
Isuru Udukala <iudukala@gmail.com>
2023-04-14 11:56:15 +0530

    adding to jsdocs

--- src/persistence/commitToPersistence.ts
+++ src/persistence/commitToPersistence.ts
@@ -2,16 +2,14 @@ import mongoose from 'mongoose';
 import { Fruit } from '../Fruit/Fruit.js';
 import { connectDB } from './connectDB.js';
 import { mapToPersistenceModel } from './mapToPersistenceModel.js';
-import { FruitGQLDocType } from './type_FruitGQLDocType.js';
 import { FruitTypeGQL } from '../graphql/nexus_types/FruitTypeGQLNX.js';
 
 /**
  * takes a fruit and commits it to the database. would make sense to convert this to a generic function but currently the only domain entity is fruit.
  *
- * @param fruit fruit object
- * @param DB_URI database connection uri
- *
- * @returns the committed object cast to a form that the nexus resolvers recognize
+ * @param {Fruit} fruit fruit object
+ * @param {string} DB_URI database connection uri
+ * @returns {Promise<FruitTypeGQL>} the committed object cast to a form that the nexus resolvers recognize
  */
 export async function commitToPersistence(fruit: Fruit, DB_URI: string): Promise<FruitTypeGQL> {
 	connectDB(DB_URI);
