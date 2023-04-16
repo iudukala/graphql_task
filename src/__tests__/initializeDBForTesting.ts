e84aed1eca923a5159759d175d53fbabe46aa590
Isuru Udukala <iudukala@gmail.com>
2023-04-16 09:57:39 +0530

    fixed database write fail bug

--- src/__tests__/initializeDBForTesting.ts
+++ src/__tests__/initializeDBForTesting.ts
@@ -6,7 +6,7 @@ import { commitToPersistence } from '../persistence/commitToPersistence.js';
 import { connectDB } from '../persistence/connectDB.js';
 import { mapToPersistenceModel } from '../persistence/mapToPersistenceModel.js';
 import mongoose from 'mongoose';
-import { MongoServerError } from 'mongodb';
+import { MongoError, MongoServerError } from 'mongodb';
 
 /**
  * clears out data and adds
@@ -14,30 +14,16 @@ import { MongoServerError } from 'mongodb';
  * @param DB_URI database connection uri
  */
 export async function initializeDBForTesting(DB_URI: string | null | undefined) {
-	// dotenv.config();
 	await connectDB(DB_URI);
 
 	try {
 		await FruitModel.collection.drop();
 	} catch (exception) {
-		// console.log(exception);
-		// i[f (exception instanceof MongoServerError)
+		if (exception instanceof MongoError) console.log(exception);
 		// else throw exception;
 	} finally {
-		// mongoose.connection.on('connected', async () => {
-		// 	await FruitModel.insertMany([...tempDataFruit.map(mapToPersistenceModel)]);
-		// });
 		await FruitModel.insertMany([...tempDataFruit.map(mapToPersistenceModel)]);
 	}
-	// await FruitModel.collection.drop();
-	// FruitModel.insertMany([...tempDataFruit.map(mapToPersistenceModel)]);
 
 	mongoose.connection.close();
-
-	// try {
-	// 	await FruitModel.collection.drop();
-	// } finally {
-	// 	FruitModel.insertMany([...tempDataFruit.map(mapToPersistenceModel)]);
-	// 	// tempDataFruit.forEach(fruit => commitToPersistence(fruit, contextGQL.DB_URI));
-	// }
 }
