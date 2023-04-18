a9f5c8f6c2f939b2e2fa0787e8a27ae22a4a13fd
Isuru Udukala <iudukala@gmail.com>
2023-04-18 23:54:18 +0530

    removed isolated repository functions

--- src/persistence/connectDB.ts
+++ src/persistence/connectDB.ts
@@ -1,24 +1,18 @@
 import mongoose from 'mongoose';
 
 /**
- * function to reconnect to db if it's closed/closing
- *
- * @param {string} DB_URI connection uri
+ * @description function to reconnect to db if it's closed/closing
+ * @param DB_URI connection uri
  */
 export const connectDB = async (DB_URI: string | null | undefined) => {
 	if (DB_URI === null || DB_URI === undefined) throw new Error('database connection string empty');
 
 	try {
 		// if in state 'disconnected' = 0 or 'disconnecting' = 3
-		// console.log('connectDB pre con : ' + mongoose.connection.readyState);
-
 		if (mongoose.connection.readyState !== 1 && mongoose.connection.readyState !== 2) {
 			await mongoose.connect(DB_URI);
-			// mongoose.createConnection(DB_URI);
-
-			// console.log('connectDB - should work : ' + mongoose.connection.readyState);
 		}
 	} catch (error) {
-		// console.log('database connection failure: ' + error);
+		console.log('database connection failure: ' + error);
 	}
 };
