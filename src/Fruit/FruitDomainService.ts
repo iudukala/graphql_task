0d13408a9c9e2e0f50f98a4b8595693a6985591c
Isuru Udukala <iudukala@gmail.com>
2023-04-19 15:51:47 +0530

    implementing fruit service

--- src/Fruit/FruitDomainService.ts
+++ src/Fruit/FruitDomainService.ts
@@ -1,5 +1,16 @@
-class FruitDomainService {
-	public static ensureUnique(fruitName: string, DB_URI: string): boolean {
-		return false;
+import { FruitRepo } from './FruitRepository.js';
+import { FruitModelType } from './types.js';
+
+export class FruitService {
+	private readonly repo: FruitRepo;
+
+	constructor(repo: FruitRepo) {
+		this.repo = repo;
+	}
+
+	public async ensureUnique(fruitName: string): Promise<boolean> {
+		const target: FruitModelType = await this.repo.findFruitByName(fruitName);
+
+		return !!target;
 	}
 }
