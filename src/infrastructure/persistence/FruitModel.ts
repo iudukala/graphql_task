3d33b117f1cbf3859bb1a47afa3423b40f5d5c1a
Isuru Udukala <iudukala@gmail.com>
2023-04-22 20:19:42 +0530

    minor refactor on fruit model collection name

--- src/infrastructure/persistence/FruitModel.ts
+++ src/infrastructure/persistence/FruitModel.ts
@@ -2,12 +2,12 @@ import mongoose from 'mongoose';
 import { FruitModelType, FruitDTO } from '../../Fruit/types.js';
 
 export const FruitModel = mongoose.model<FruitModelType>(
-	'Fruit',
+	'fruit',
 	new mongoose.Schema<FruitDTO>({
 		name: String,
 		description: String,
 		limit: Number,
 		amount: Number,
 	}),
-	'Fruit',
+	'fruit',
 );
