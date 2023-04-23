8e04bc895be7575d2f5d2e320fd00725df1e235e
Isuru Udukala <iudukala@gmail.com>
2023-04-24 02:28:39 +0530

    finalizing test updates

--- src/__tests__/updateFruitForFruitStorage.test.ts
+++ src/__tests__/updateFruitForFruitStorage.test.ts
@@ -1,4 +1,6 @@
 import { FruitDTO } from '../Fruit/types.js';
+import { CREATE_LEMON_MUTATION } from './data/CREATE_LEMON_MUTATION_STR.js';
+import { MUTATION_NAME } from './data/MUTATION_NAME.js';
 import { perfromQuery } from './helpers/performQuery.js';
 
 describe('storeFruitToFruitStorage() endpoint test', () => {
@@ -15,22 +17,12 @@ describe('storeFruitToFruitStorage() endpoint test', () => {
 	});
 
 	test('create valid fruit', async () => {
-		const CREATE_MUT_NAME = 'createFruitForFruitStorage';
-		await perfromQuery(
-			`mutation{
-				${CREATE_MUT_NAME}(
-					name: "lemon", description: "this is a lemon", limit: 10){
-						name
-						limit
-						description
-				}
-			}`,
-		).then(result => {
-			const returned = result.data?.[CREATE_MUT_NAME] as FruitDTO;
+		await perfromQuery(CREATE_LEMON_MUTATION).then(result => {
+			const returned = result.data?.[MUTATION_NAME.CREATE] as FruitDTO;
 
 			expect(returned.name).toBe('lemon');
 			expect(returned.limit).toBe(10);
-			expect(returned.description).toBe('this is a lemon');
+			expect(returned.amount).toBe(0);
 		});
 	});
 
--- src/__tests__/updateFruitForFruitStorage_neg.test.ts
+++ src/__tests__/updateFruitForFruitStorage_neg.test.ts
@@ -1,4 +1,6 @@
 import { FruitDTO } from '../Fruit/types.js';
+import { CREATE_LEMON_MUTATION } from './data/CREATE_LEMON_MUTATION_STR.js';
+import { MUTATION_NAME } from './data/MUTATION_NAME.js';
 import { perfromQuery } from './helpers/performQuery.js';
 
 describe('storeFruitToFruitStorage() endpoint test', () => {
@@ -15,30 +17,19 @@ describe('storeFruitToFruitStorage() endpoint test', () => {
 	});
 
 	test('create valid fruit', async () => {
-		const CREATE_MUT_NAME = 'createFruitForFruitStorage';
-		await perfromQuery(
-			`mutation{
-				${CREATE_MUT_NAME}(
-					name: "lemon", description: "this is a lemon", limit: 10){
-						name
-						limit
-						description
-				}
-			}`,
-		).then(result => {
-			const returned = result.data?.[CREATE_MUT_NAME] as FruitDTO;
+		await perfromQuery(CREATE_LEMON_MUTATION).then(result => {
+			const returned = result.data?.[MUTATION_NAME.CREATE] as FruitDTO;
 
 			expect(returned.name).toBe('lemon');
 			expect(returned.limit).toBe(10);
-			expect(returned.description).toBe('this is a lemon');
+			expect(returned.amount).toBe(0);
 		});
 	});
 
 	test('update to a valid description', async () => {
-		const UPDATE_MUT_NAME = 'updateFruitForFruitStorage';
 		await perfromQuery(
 			`mutation{
-				${UPDATE_MUT_NAME}(
+				${MUTATION_NAME.UPDATE}(
 					name: "lemon", description: "updated lemon with a long description"){
 						name
 						description
